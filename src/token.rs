77f6290337ccaa7eeb5b48f5cb66a25904a4060a
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-22 21:11:34 -0400

    Position structure

--- src/token.rs
+++ src/token.rs
@@ -14,3 +14,22 @@ pub enum TokenType {
     Integer(i32),
     Float(f32)
 }
+
+#[derive(PartialEq, Debug, Clone)]
+pub struct Position {
+    pub index: usize,
+    pub column: usize,
+    pub line: usize
+}
+
+impl Position {
+    pub fn advance(&mut self, character: char) {
+        self.index += 1;
+        self.column += 1;
+
+        if character == '\n' {
+            self.line += 1;
+            self.column = 0;
+        }
+    }
+}
