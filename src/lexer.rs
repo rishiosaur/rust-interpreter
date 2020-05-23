bf53bb3fcf0cda4ce2962091d04f2a38204502ed
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-22 21:11:22 -0400

    Rest of lexing

--- src/lexer.rs
+++ src/lexer.rs
@@ -30,4 +30,80 @@ impl Lexer {
             self.current_char = Some(self.text.as_bytes()[self.position.index] as char);
         }
     }
+
+    fn integer (&mut self) -> Token {
+        println!("Integer detected");
+        let mut digits = String::new();
+
+        // Counting points for floating-points
+        let mut dots = 0;
+
+        while self.current_char != None && (self.current_char.unwrap().is_digit(10) || self.current_char.unwrap() == '.') {
+            println!("Character detected");
+            if self.current_char.unwrap() == '.' {
+                println!("Dot detected");
+                dots += 1;
+                digits.push('.');
+                self.advance();
+            } else {
+                println!("Number detected");
+                digits.push(self.current_char.unwrap());
+                self.advance();
+            }
+        }
+
+        if dots > 0 {
+            Token { token_type:  Float(digits.parse::<f32>().unwrap()) }
+        } else {
+            Token { token_type: Integer(digits.parse::<i32>().unwrap()) }
+        }
+
+    }
+
+    pub fn lex (&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = vec![];
+
+        println!("Making");
+
+        while self.current_char != None {
+            println!("Character detected");
+            match self.current_char.unwrap() {
+                char if char.is_whitespace() => self.advance(),
+
+                // Operators
+                '+' => {
+                    tokens.push(Token { token_type: Add });
+                    self.advance();
+                }
+
+                '-' => {
+                    tokens.push(Token { token_type: Subtract });
+                    self.advance();
+                }
+
+                '*' => {
+                    tokens.push(Token { token_type: Multiply });
+                    self.advance();
+                }
+
+                '/' => {
+                    tokens.push(Token { token_type: Divide });
+                    self.advance();
+                }
+
+
+                // Digit
+                char if char.is_digit(10) => {
+                    tokens.push(self.integer());
+                    self.advance();
+                },
+
+                _ => {
+                    panic!(format!("Error in parsing at position: {:#?}", self.position))
+                }
+            }
+        }
+
+        tokens
+    }
  }
