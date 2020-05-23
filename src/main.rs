bbe4c13515bd251ebcb1e74dfe5062d4c33aa8d1
Rishi Kothari <itsrishikothari@gmail.com>
2020-05-22 21:11:54 -0400

    Rewrite main

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod token;
+mod lexer;
+
+use lexer::Lexer;
+
+fn main() {
+    let text = "288.80000 * 34.988 + 32 - 4".into();
+    let mut lexer = Lexer::new(text);
+
+    println!("{:#?}", lexer.lex())
+}
